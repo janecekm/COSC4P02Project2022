1b52f6f4c0f590ec806e032d5045022b9209e5c9
Joel-Liju <69165450+Joel-Liju@users.noreply.github.com>
2022-01-23 14:11:01 -0500

    Add files via upload

---
+++ web-scraping/academicadvisor.py
@@ -0,0 +1,20 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+driver.get("https://brocku.ca/academic-advising/find-your-advisor/")
+driver.title
+MainContent = []
+maincontent = driver.find_element(By.ID, "page-content")
+rows = maincontent.find_elements(By.TAG_NAME,"tr")
+for row in rows:
+    values = row.find_elements(By.TAG_NAME,"td")
+    temp = []
+    if values[0].text == "Department" or values[0].text == "Advisor":
+        continue
+    for value in values:
+        temp.append(value.text)
+    MainContent.append(temp)
+print(MainContent)
+driver.close()
+    
