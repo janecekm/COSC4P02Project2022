782620b5faab0754202595a9f146e0449f9a5efd
janecekm <madeline@janecek.ca>
2022-01-29 00:26:03 -0500

    added input bar

--- chatbot/src/Chatbox/Form/form.js
+++ chatbot/src/Chatbox/Form/form.js
@@ -4,9 +4,22 @@ import "./form.css"
 function Form (){
     const [open, setopen] = useState(false);
     return (
-        <div className='Form'>
-            Input bar (coming soon!)
-        </div>
+        <form className="form">
+            <input
+                    className="input"
+                    type="text"
+                    placeholder="Type a query here..."
+
+                    /*value={message}
+                    
+                    onChange={({ target: { value } }) => setMessage(value)}
+                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+                    <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
+                    */
+                />
+            
+            
+        </form>
     )
 }
 export default Form;
