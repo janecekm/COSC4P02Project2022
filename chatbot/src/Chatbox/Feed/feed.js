0563a00e60184255bed5ee2fb2f7401094cacfcd
Madeline Janecek <madeline@janecek.ca>
2022-02-02 14:19:36 -0500

    Added chat bubble and enter key

--- chatbot/src/Chatbox/Feed/feed.js
+++ chatbot/src/Chatbox/Feed/feed.js
@@ -5,33 +5,43 @@ import ReactDOM from "react-dom";
 class Message extends React.Component {
   render() {
     return (
-      <div>{this.props.text}</div>
+      <div className={this.props.type}>{this.props.text}</div>
     );
   }
 };
 
 const Feed = () => {
-  const [inputList, setInputList] = useState([]);
-  const onAddBtnClick = event => {
+  const [messagesList, setMessagesList] = useState([]);
+  const poseQuery = ()  => {
     var query = document.getElementById("inputField").value;
     if (query != "") {
-      setInputList(inputList.concat(<Message key={inputList.length} text = {query} />));
+      setMessagesList(
+        messagesList.concat(<Message key={messagesList.length} text = {query} type = "user_message"/>)
+      );
       clearInput();
     }
   };
+
+
   const clearInput = () => {
     document.getElementById("inputField").value = "";
   }
-  
+  const handler = (event) => {
+    if (event.key === "Enter") {
+      poseQuery();
+    }
+  }
+
   return (
     <div>
       <div className='feed'>
-        {inputList}
+        {messagesList}
       </div>
-      <button onClick={onAddBtnClick}>Enter</button>
-      <input id = "inputField" placeholder="Type a query here..."></input>
-      <button onClick={clearInput}>Clear</button>
+      <button className="clearButton" onClick={clearInput}>Clear</button>
+      <input className = "inputBar" onKeyPress={(e) => handler(e)} id = "inputField" placeholder="Type a query here..."></input>
+      <button className="enterButton" onClick={poseQuery}>Enter</button>
     </div>
+
   );
 };
 
