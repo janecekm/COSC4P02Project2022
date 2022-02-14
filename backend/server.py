750b97b9ea15279393e8cbd01acb7d3d30a48427
Joel-Liju <69165450+Joel-Liju@users.noreply.github.com>
2022-02-14 14:48:22 -0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,16 @@
-from flask import Flask
+import json
+from flask import Flask, request
 
 app = Flask(__name__)
+@app.route('/',methods = ['GET'])
+def tester():
+    return "hello this is flask and this is getting thigs"
 
-@app.route("/")
-def front():
-    print("test")
-    return "test"
+@app.route('/', methods =['POST'])
+def test():
+    t = json.loads(request.data)
+    print(t['message'])
+    return ">test<"
 
 if __name__ == "__main__":
-    app.run(debug=True,port=4000)
+    app.run(debug=True, port=5000)
