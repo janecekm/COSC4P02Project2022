57592f16382b0da70d955562ca2fb02403ee61d4
gpog <gregpogue2@hotmail.com>
2022-03-13 13:56:22 -0400

    exception handling and additional keyword queries

--- backend/queryTables.py
+++ backend/queryTables.py
@@ -7,12 +7,34 @@ from flask_sqlalchemy import SQLAlchemy
 def doQueries(keywords):
     db = server.db
     # print(keywords)
-    if 'course code' and ('prereqs' or 'description') in keywords:
-        print(keywords.get('course code'))
-        filterCourseInputs(keywords)
-        # temp = server.Course.query.filter_by(code='COSC4P03').first()
-        # print(temp.description)
-        print(server.Course.query.filter_by(code=keywords.get('course code')).first())
+    if 'prereqs' in keywords or 'general question' in keywords or 'xlist' in keywords:
+        try:
+            print(keywords)
+            print(keywords.get('course code'))
+            filterCourseInputs(keywords)
+            # temp = server.Course.query.filter_by(code='COSC4P03').first()
+            # print(temp.description)
+            print(server.Course.query.filter_by(code=keywords.get('course code')).first())
+        except AttributeError:
+            print('Attribute Error')
+            return 'more info required'
+        except:
+            print('im in danger')
+            return 'im in danger'
+    elif 'location' in keywords:
+        print(keywords)
+        try:
+            if 'course code' in keywords:
+                filterCourseInputs(keywords)
+            elif 'building' in keywords:
+                print('building found')
+                return 'placeholder return'
+            else:
+                print('more info required')
+                return 'more info required'
+        except:
+            print('location not found')
+            return 'not found'
     # print(server.Course.query.all())
     return 'placeholder return'
 
@@ -20,6 +42,6 @@ def doQueries(keywords):
 def filterCourseInputs(keywords):
     temp = keywords.get('course code').text
     temp = temp.upper()
-    print("filtered input: "+temp)
+    print('filtered input: '+temp)
     keywords['course code'] = temp
     return None
