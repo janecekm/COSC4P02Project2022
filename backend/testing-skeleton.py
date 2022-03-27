1dad7abd085c7ff890053b82b145a8fc70fce9e2
Ky-Sch <65508122+Ky-Sch@users.noreply.github.com>
2022-03-27 16:47:21 -0400

    Update testing-skeleton.py

--- backend/testing-skeleton.py
+++ backend/testing-skeleton.py
@@ -3,4 +3,4 @@ import botNLP
 def testing_invalid_input_ProcessQ():
     assert botNLP.processQ('raietweiaweeiiwnaeiugwe')['message'] == "I am not quite sure what you're asking. Could you rephrase that?"
 def testing_valid_input_ProcessQ():
-    assert botNLP.processQ('What are the prereqs for COSC 1P02')['message'].contains("COSC 1P03")
+    assert botNLP.processQ('What are the prereqs for COSC 1P03')['message'].contains("COSC 1P02")
