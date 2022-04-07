9e33a8240b58130fa0f4a98285f05418d5af40a6
Dare <joelliju10@gmail.com>
2022-04-07 17:32:45 -0400

    more tests added

--- backend/testing-skeleton.py
+++ backend/testing-skeleton.py
@@ -1,3 +1,5 @@
+from numpy import extract
+from py import process
 import botNLP
 
 error_message_1 = "I'm sorry, I wasn't able to find what you were looking for. However, you might be able to find more information at: https://brocku.ca/"
@@ -19,6 +21,9 @@ def testing_attempt_SQL_injection2_ProcessQ():
     assert (botNLP.processQ('What are the prereqs for /**/ *’--')['message'] == error_message_1) or (botNLP.processQ('What are the prereqs for /**/ *’--')['message'] == error_message_2) 
 def testing_single_hello_processQ():
     assert botNLP.processQ('Hello Hello')['message']== "Hello"
+
+def testing_spacefor_course():
+    assert 'MATH 1P66' in botNLP.processQ("what are prereq for math1p67")['message']
 
 #botNLP.getLink
 def testing_prereq_getLink():
@@ -165,3 +170,73 @@ def testing_extract_keywords_tuition2():
 def testing_extract_keywords_xlist():
     response = botNLP.extractKeywords("What is crosslist for COSC 4P61?")
     assert (12057252092477718455, 0,3) in response[0]
+
+'''
+testing for process Keywords
+'''
+def testing_description_of_course():
+    matches, doc = botNLP.extractKeywords("what is cosc 1p03?")#spelt correct
+    temp2 = botNLP.processKeywords(matches, doc)
+    assert temp2["description"].text == "what is" and temp2["code"].text =="cosc 1p03"
+    matches, doc = botNLP.extractKeywords("WHAT IS COSC 2P03")#all in caps
+    temp2 = botNLP.processKeywords(matches,doc)
+    assert temp2["description"].text == "what is" and temp2["code"].text == "cosc 2p03"
+
+def testing_spaces_between_course():
+    matches, doc = botNLP.extractKeywords("what are prereq for math1p67?")
+    temp = botNLP.processKeywords(matches, doc)
+    assert temp["prereq"].text == "what are prereq" and temp["code"].text == "math 1p67"
+
+def testing_processing_speltwrong():
+    matches, doc = botNLP.extractKeywords("what are prereq for Math 1p67?")
+    temp2 = botNLP.processKeywords(matches,doc)
+    assert temp2["prereq"].text == "what are prereq" and temp2["code"].text == "math 1p67"
+
+##need to test crosslisting, and need to know how to test it.
+
+def testing_processing_exam():
+    matches, doc = botNLP.extractKeywords("where is my cosc 4p32 exam")
+    temp = botNLP.processKeywords(matches,doc)
+    assert temp["exam"].text == "exam" and temp["code"].text == "cosc 4p32"
+
+def testing_processing_crosslisting():
+    matches, doc = botNLP.extractKeywords("what is COSC 4p61 crosslisted as?")
+    temp = botNLP.processKeywords(matches, doc)
+    assert temp["xlist"].text == "crosslisted" and temp["code"].text == "cosc 4p61"
+    matches, doc = botNLP.extractKeywords("what are the crosslisting of Cosc 4p61")
+    temp = botNLP.processKeywords(matches,doc)
+    assert temp["xlist"].text == "crosslisted" and temp["code"].text == "cosc 4p61"
+
+def testing_processing_location_courses():
+    matches, doc = botNLP.extractKeywords("where is cosc 3p98 class")
+    temp = botNLP.processKeywords(matches,doc)
+    assert temp["location"].text == "where" and temp["code"].text =="cosc 3p98"
+
+def testing_processing_instructor_courses():
+    matches, doc = botNLP.extractKeywords("who teaches math 1p66?")
+    temp = botNLP.processKeywords(matches, doc)
+    assert temp["instructor"].text == "who teaches" and temp["code"].text == "math 1p66"
+
+def testing_processing_time_courses():
+    matches, doc = botNLP.extractKeywords("when is econ 2p30 class")
+    temp = botNLP.processKeywords(matches, doc)
+    assert temp["time"].text == "when is" and temp["code"].text == "econ 2p30"
+
+def testing_processing_time_courses_lab():
+    matches, doc = botNLP.extractKeywords("when is econ 2p30 lab")
+    temp = botNLP.processKeywords(matches,doc)
+    assert temp["course component"].text == "lab" and temp["time"].text == "when is" and temp["code"].text == "econ 2p30"
+
+def testing_processing_building():
+    matches, doc = botNLP.extractKeywords("where is MCJ")
+    temp = botNLP.processKeywords(matches,doc)
+    assert temp["location"].text == "where" and temp["buildingCode"].text == "mcj"
+    matches, doc = botNLP.extractKeywords("How can I get to MCD")
+    temp = botNLP.processKeywords(matches,doc)
+    assert temp["buildingCode"].text == "mcd"
+    matches, doc = botNLP.extractKeywords("where is mcd 205")
+    temp = botNLP.processKeywords(matches,doc)
+    assert temp["buildingCode"].text == "mcd"
+    matches, doc = botNLP.extractKeywords("where is mcd205")
+    temp = botNLP.processKeywords(matches,doc)
+    assert temp["buildingCode"].text == "mcd"
