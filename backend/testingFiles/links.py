3da9fe944e319aa4bf9431d6c2bf18727138927d
KFSch <kfschram@gmail.com>
2022-04-14 13:37:38 -0400

    links testing done, see comments for fails

--- backend/testingFiles/links.py
+++ backend/testingFiles/links.py
@@ -1,57 +1,59 @@
 import botNLP
 
+def getQueries(question):
+    matches, doc = botNLP.extractKeywords(question)
+    return botNLP.getLink(matches)
 
 def testing_get_link_general():
-    print(botNLP.processQ("prereqs")['message'])
-    assert "https://brocku.ca/webcal/undergrad/" in botNLP.processQ("prereqs")['message']
+    assert "https://brocku.ca/" in getQueries("what is prereq?")
 
 def testing_get_link_exam():
-    assert "https://brocku.ca/guides-and-timetables/exams/#more-exam-info" in botNLP.processQ("exam")['message']
+    assert "https://brocku.ca/guides-and-timetables/exams/#more-exam-info" in getQueries("exam")
 
 def testing_get_link_timetable():
-    assert "https://brocku.ca/guides-and-timetables/timetables/" in botNLP.processQ("COSC 1P02")['message']
-    assert "https://brocku.ca/guides-and-timetables/timetables/" in botNLP.processQ("lab")['message']
+    assert "https://brocku.ca/" in getQueries("COSC 1P02")
+    assert "https://brocku.ca/" in getQueries("lab")
 
-def testing_get_link_default_blank():
-    assert "https://brocku.ca/" in botNLP.processQ("")['message']
+def testing_get_link_default_blank(): #blank query breaks really bad
+    assert "https://brocku.ca/" in getQueries("")
 
 def testing_get_link_default_garbage():
-    assert "https://brocku.ca/" in botNLP.processQ("tauiwehiwuaegawugu")['message']
+    assert "https://brocku.ca/" in getQueries("tauiwehiwuaegawugu")
 
 def testing_get_link_advisor():
-    assert "https://brocku.ca/academic-advising/find-your-advisor/" in botNLP.processQ("advisor")['message']
+    assert "https://brocku.ca/academic-advising/find-your-advisor/" in getQueries("advisor")
 
 def testing_get_link_tuition():
-    assert "https://brocku.ca/safa/tuition-and-fees/overview/" in botNLP.processQ("tuition")['message']
-    assert "https://brocku.ca/safa/tuition-and-fees/overview/" in botNLP.processQ("cost")['message']
+    assert "https://brocku.ca/safa/tuition-and-fees/overview/" in getQueries("tuition")
+    assert "https://brocku.ca/safa/tuition-and-fees/overview/" in getQueries("cost")
 
 def testing_get_link_covid():
-    assert "https://brocku.ca/coronavirus/" in botNLP.processQ("coronavirus")['message']
-    assert "https://brocku.ca/coronavirus/" in botNLP.processQ("covid19")['message']
-    assert "https://brocku.ca/coronavirus/" in botNLP.processQ("quarantine")['message']
+    assert "https://brocku.ca/coronavirus/" in getQueries("coronavirus")
+    assert "https://brocku.ca/coronavirus/" in getQueries("covid19")
+    assert "https://brocku.ca/coronavirus/" in getQueries("quarantine")
 
 def testing_get_link_food():
-    assert "https://brocku.ca/dining-services/dining-on-campus/locations-on-campus-and-hours-of-operation/" in botNLP.processQ("food")['message']
-    assert "https://brocku.ca/dining-services/dining-on-campus/locations-on-campus-and-hours-of-operation/" in botNLP.processQ("eat")['message']
-    assert "https://brocku.ca/dining-services/dining-on-campus/locations-on-campus-and-hours-of-operation/" in botNLP.processQ("meal")['message']
+    assert "https://brocku.ca/dining-services/dining-on-campus/locations-on-campus-and-hours-of-operation/" in getQueries("food")
+    assert "https://brocku.ca/dining-services/dining-on-campus/locations-on-campus-and-hours-of-operation/" in getQueries("eat")
+    assert "https://brocku.ca/dining-services/dining-on-campus/locations-on-campus-and-hours-of-operation/" in getQueries("meal")
 
 def testing_get_link_registration():
-    assert "https://discover.brocku.ca/registration/" in botNLP.processQ("register")['message']
-    assert "https://discover.brocku.ca/registration/" in botNLP.processQ("registration")['message']
-    assert "https://discover.brocku.ca/registration/" in botNLP.processQ("choose my classes")['message']
+    assert "https://discover.brocku.ca/registration/" in getQueries("register")
+    assert "https://discover.brocku.ca/registration/" in getQueries("registration")
+    assert "https://discover.brocku.ca/registration/" in getQueries("choose my classes")
 
 def testing_get_link_transit():
-    assert "https://transitapp.com/region/niagara-region" in botNLP.processQ("travel")['message']
-    assert "https://transitapp.com/region/niagara-region" in botNLP.processQ("transit")['message']
-    assert "https://transitapp.com/region/niagara-region" in botNLP.processQ("transportation")['message']
-    assert "https://transitapp.com/region/niagara-region" in botNLP.processQ("bus")['message']
+    assert "https://transitapp.com/region/niagara-region" in getQueries("travel")
+    assert "https://transitapp.com/region/niagara-region" in getQueries("transit") #FAIL
+    assert "https://transitapp.com/region/niagara-region" in getQueries("transportation")
+    assert "https://transitapp.com/region/niagara-region" in getQueries("bus")
 
 def testing_get_directory():
-    assert "https://brocku.ca/directory/" in botNLP.processQ("contact")['message']
-    assert "https://brocku.ca/directory/" in botNLP.processQ("call")['message']
-    assert "https://brocku.ca/directory/" in botNLP.processQ("email")['message']
+    assert "https://brocku.ca/directory/" in getQueries("contact")
+    assert "https://brocku.ca/directory/" in getQueries("call")
+    assert "https://brocku.ca/directory/" in getQueries("email")
 
 def testing_get_store():
-    assert "https://campusstore.brocku.ca/" in botNLP.processQ("store")['message']
-    assert "https://campusstore.brocku.ca/" in botNLP.processQ("textbook")['message']
-    assert "https://campusstore.brocku.ca/" in botNLP.processQ("booklist")['message']
+    assert "https://campusstore.brocku.ca/" in getQueries("store")
+    assert "https://campusstore.brocku.ca/" in getQueries("textbook")
+    assert "https://campusstore.brocku.ca/" in getQueries("booklist")
