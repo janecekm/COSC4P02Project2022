49bc01a57f8f4b3e97f527a89f7ad306d1cd48c0
Dare <joelliju10@gmail.com>
2022-04-17 20:14:46 -0400

    made significant improvements to splash page

--- backend/testing-skeleton.py
+++ backend/testing-skeleton.py
@@ -1,6 +1,6 @@
 import os
 import pytest
-
+from pytest import ExitCode
 #now we need to run python3 testing-skeleton
 def filepath():
     if os.path.basename(os.getcwd()) =="backend":#we are in COSC4p02Project2022/backend
@@ -12,4 +12,5 @@ list = []
 for name in os.listdir(filepath()):
     list.append(filepath()+name)
 
-pytest.main(list)
+extc = pytest.main(list)
+raise SystemExit(extc)
